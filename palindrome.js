9d4127674cabf2c05654b7c2a020b6d153ff481c
Preetykumari06 <preetysingh46231@gmail.com>
2022-12-05 17:10:20 +0530

    added prime and palindrome problem

---
+++ palindrome.js
@@ -0,0 +1,17 @@
+let str="aman";
+
+function reverse(string){
+  let bag="";
+  for(let i=string.length-1; i>=0; i--){
+    bag=bag+string[i];
+  }
+    return bag;
+}
+
+let rev_str=reverse(str);
+
+if(str==rev_str){
+  console.log("Palindrome");
+} else {
+  console.log("NA");
+}
